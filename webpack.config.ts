2984cea195786de61c12c50297388150e2b9d9ab
SasakiPeter <anagentofthegod@gmail.com>
2021-04-24 14:19:43 +0900

    :wrench: add types at webpack.config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,20 @@
-const { resolve } = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import type { Configuration } from "webpack";
+import type { Configuration as DevConfiguration } from "webpack-dev-server";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { resolve } from "path";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-module.exports = {
+declare module "webpack" {
+  interface Configuration {
+    devServer?: DevConfiguration;
+  }
+}
+
+const config: Configuration = {
   mode: isProduction ? "production" : "development",
   entry: {
-    index: resolve("src", "index.tsx"),
+    index: resolve(__dirname, "src", "index.tsx"),
   },
   output: {
     path: resolve(__dirname, "dist"),
@@ -14,7 +22,7 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    port: "3000",
+    port: 3000,
     hot: true,
     open: true,
   },
@@ -36,7 +44,7 @@ module.exports = {
           {
             loader: "ts-loader",
             options: {
-              configFile: resolve(__dirname, "tsconfig.json"),
+              configFile: resolve(__dirname, "src", "tsconfig.json"),
             },
           },
         ],
@@ -63,7 +71,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       minify: isProduction,
-      template: resolve("src", "index.html"),
+      template: resolve(__dirname, "src", "index.html"),
     }),
   ],
 };
+
+export default config;
